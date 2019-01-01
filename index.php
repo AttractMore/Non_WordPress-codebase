2c47f55ed32727b011c0fb03a4474d0f9df94bf8
AttractMore <roger@attractmore.co.uk>
2019-01-01 09:20:37 +0100

    Initial Commit

---
+++ index.php
@@ -0,0 +1,136 @@
+<!doctype html>
+<html lang="en-GB" prefix="og: http://ogp.me/ns#">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="Content-Type" content="text/html" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="description" content="site desicription" />
+		<link rel="canonical" href="https://" />
+		<link href="/css/style.css" type="text/css" rel="stylesheet" media="all">
+		<title>Non-Wordpress Standard Codebase</title>
+
+		<meta name="viewport" content="width=device-width, initial-scale=1"/>
+		<meta name="HandheldFriendly" content="True">
+		<meta name="MobileOptimized" content="320">
+		<meta name="mobile-web-app-capable" content="yes">
+		<meta name="apple-mobile-web-app-capable" content="yes">
+		<meta name="robots" content="noodp,noindex"/>
+
+		<meta name="dc.language" content="en-GB" />
+		<meta name="dcterms.rightsHolder" content="AttractMore" />
+		<meta name="dc.publisher" content="AttractMore" />
+		<meta name="dc.title" content="" />
+		<meta name="dc.description" content="" />
+		<meta name="dc.date.issued" content="2017-04-11" />
+
+		<meta property="og:locale" content="en_GB" />
+		<meta property="og:type" content="website" />
+		<meta property="og:title" content="Home &bull; AttractMore" />
+		<meta property="og:description" content="site desicription" />
+		<meta property="og:url" content="https://attractmore.uk/" />
+		<meta property="og:site_name" content="AttractMore" />
+		<meta property="og:image" content="https://attractmore.uk/wp-content/uploads/reaching-for-the-sky-1300x500.jpg" />
+
+		<meta name="twitter:card" content="summary" />
+		<meta name="twitter:description" content="site desicription" />
+		<meta name="twitter:title" content="Home &bull; AttractMore" />
+		<meta name="twitter:site" content="@AttractMoreWeb" />
+		<meta name="twitter:image" content="https://attractmore.uk/wp-content/uploads/reaching-for-the-sky-1300x500.jpg" />
+		<meta name="twitter:creator" content="@AttractMoreWeb" />
+
+		<script type='application/ld+json'>{"@context":"http:\/\/schema.org","@type":"WebSite","@id":"#website","url":"https:\/\/attractmore.uk\/","name":"AttractMore","potentialAction":{"@type":"SearchAction","target":"https:\/\/attractmore.uk\/?s={search_term_string}","query-input":"required name=search_term_string"}}</script> 
+		<script type='application/ld+json'>
+		{"@context":"http:\/\/schema.org","@type":"Organization","url":"https:\/\/attractmore.uk\/","sameAs":["https:\/\/www.facebook.com\/pages\/Attractmore-Web-Design\/5879297977","https:\/\/www.linkedin.com\/in\/rogerknight","https:\/\/www.youtube.com\/channel\/UCdSzyKaL7Ycn2p-3IjhaM9A","https:\/\/twitter.com\/AttractMoreWeb"],"@id":"#organization","name":"AttractMore Limited","logo":"https:\/\/attractmore.uk\/wp-content\/uploads\/attractmore-logo-mark.png"}</script> 
+		
+	</head>
+
+	<body itemscope itemtype="http://schema.org/WebPage">
+		<div id="container">
+			<header class="header" role="banner" itemscope itemtype="http://schema.org/WPHeader">
+				<div id="inner-header" class="wrap cf">
+					<p id="logo" class="h1" itemscope itemtype="http://schema.org/Organization">
+						<a href="https://attractmore.uk" rel="nofollow">AttractMore</a>
+					</p>
+					<nav role="navigation" itemscope itemtype="http://schema.org/SiteNavigationElement" class="header-nav">
+						<ul id="menu-main-menu" class="nav nav-collapse top-nav cf">
+							<li><a href="https://attractmore.uk/services/">Services</a></li>
+							<li><a href="https://attractmore.uk/services/">Services</a></li>
+							<li><a href="https://attractmore.uk/services/">Services</a></li>
+							<li><a href="https://attractmore.uk/services/">Services</a></li>
+							<li><a href="https://attractmore.uk/services/">Services</a></li>
+						</ul>
+					</nav>
+				</div>
+			</header>
+			<div id="content">
+				<div id="inner-content" class="wrap cf">
+					<div id="main" class="cf" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
+						<article class="cf" role="article" itemscope itemtype="http://schema.org/BlogPosting">
+							<h1>Level One Heading</h1>
+							<h2>Level Two Heading</h2>
+							<h3>Level Three Heading</h3>
+							<h4>Level Four Heading</h4>
+							<h5>Level Five Heading</h5>
+							<h6>Level Six Heading</h6>
+							<section class="cf" itemprop="articleBody">
+								<img class="alignright" src="images/carolyn-phillips-english-language-trainer-375.jpg" alt=""/>
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem optio quisquam doloribus, veritatis sunt tempore minus in ab. Ex, odit expedita. Ad voluptatem quod alias blanditiis repellendus eos nemo inventore.</p>
+								<p>Quas quibusdam maxime atque libero molestias facere nesciunt. Ut temporibus dignissimos aspernatur molestiae? Minima sed itaque qui odit, placeat? Corporis nihil ipsa omnis expedita dolorem necessitatibus, suscipit, veritatis molestias consequuntur!</p>
+								<p>Unde nisi vero error, voluptatem id in et magni modi, doloremque nam minima rem voluptas cumque ut harum laborum vitae voluptates, consectetur odit impedit culpa quas temporibus veniam dolor earum?</p>
+								<p>Illum fugiat aliquid neque nulla adipisci dicta voluptas animi suscipit id aut nesciunt aspernatur libero iste esse maiores, quibusdam sapiente rerum quae dignissimos totam cum provident ipsa placeat. Optio, ratione.</p>
+								<p>Dignissimos quam, pariatur cum unde at, fugit cumque perspiciatis rem non impedit, recusandae reprehenderit aperiam. Repellendus aliquam placeat commodi excepturi? Quidem adipisci, cupiditate a, vero porro delectus laboriosam aspernatur eius.</p>
+								<img class="alignleft" src="images/carolyn-phillips-english-language-trainer-375.jpg" alt=""/>
+								<p>Totam quasi error omnis atque aut non animi, earum placeat, quaerat quam adipisci quia facere voluptate enim, cum, esse rerum itaque aliquid nam rem. Quasi ipsum at nulla, aspernatur animi.</p>
+								<p>Ipsum suscipit cum distinctio fuga dignissimos, iste quibusdam? Laborum laudantium commodi officiis, labore aspernatur temporibus sint accusamus suscipit aliquam pariatur, cupiditate vitae quam rem ipsam nemo eos debitis, quisquam dolor.</p>
+								<p>Enim autem fuga velit quod praesentium ex atque voluptate laudantium, similique, in ratione tenetur cumque quidem consectetur animi! Molestiae autem aliquid sequi, harum nihil? Quae quas earum enim veniam, inventore!</p>
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam sit libero aut nisi, explicabo dolorem alias possimus ipsum sint odio quasi provident incidunt, soluta molestiae minus! Dignissimos numquam placeat similique.</p>
+								<p>Atque sunt, doloribus non eaque cumque possimus quos voluptatum tempore rem. Atque quod, dolore recusandae, sed non esse, odio ullam dolorem nulla veniam facilis quisquam dolorum magnam. Voluptatum, rem, ducimus.</p>
+								<p>Tempore illum repudiandae voluptatum consectetur, soluta, odio deserunt quia fuga qui facere iusto non, cupiditate minus culpa. Possimus, eius non facilis adipisci fugiat aspernatur quod in, cum soluta inventore consequatur.</p>
+								<img class="aligncenter" src="images/carolyn-phillips-english-language-trainer-375.jpg" alt=""/>
+								<p>Alias sequi dolorum, earum illo ut dolorem eius, pariatur voluptates eum cumque nemo, et aliquid quo nihil facere magnam? Est sint ratione impedit soluta unde culpa repellendus laudantium reiciendis aliquam.</p>
+								<ul>
+									<li>one</li>
+									<li>two</li>
+									<li>three</li>
+									<li>four</li>
+									<li>five</li>
+								</ul>
+								<ol>
+									<li>one</li>
+									<li>two</li>
+									<li>three</li>
+									<li>four</li>
+									<li>five</li>
+								</ol>
+
+								<blockquote>
+								Currently WordPress blockquotes are just wrapped in blockquote tags and have no clear way for the user to define a source. Maybe one day they'll be more semantic (and easier to style) like the version below.
+								</blockquote>
+								<blockquote cite="http://html5doctor.com/blockquote-q-cite/">
+									<p>HTML5 comes to our rescue with the footer element, allowing us to add semantically separate information about the quote.</p>
+									<footer>
+										<cite>
+											<a href="http://html5doctor.com/blockquote-q-cite/">Oli Studholme, HTML5doctor.com</a>
+										</cite>
+									</footer>
+								</blockquote>
+							</section>
+						</article>
+					</div>
+				</div>
+			</div>
+			<footer class="footer" role="contentinfo" itemscope itemtype="http://schema.org/WPFooter">
+				<div class="wrap cf">
+					<div class="footer-nav">
+						<div class="final-footer">
+							<p class="company">Company registered in England No. 9514840</p>
+							<p class="copyright">&copy; 2017 AttractMore Ltd.</p>
+						</div>
+					</div>
+				</div>
+			</footer>
+		</div>
+	</body>
+</html>
+
+
